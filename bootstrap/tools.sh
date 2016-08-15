532c2fab8817a470420937f6db27c1bcd70ecc09
Nemanja Simeunovic <nem@nemsim.com>
2016-08-14 19:39:00 -0500

    vim + tmux = <3

--- bootstrap/tools.sh
+++ bootstrap/tools.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
+# pattern "borrowed" from
+# https://github.com/tscheffe/dotfiles/blob/master/bootstrap/apps.sh
+program_exists() {
+  type $1 > /dev/null 2>&1
+}
+
 echo 'bootstrapping tools'
+
+#if ! program_exists derp; then
+#  echo 'you need to install derp!'
+#  echo 'derp.co/install'
+#  exit 1
+#else
+#  echo 'you have this'
+#fi
 
 # if [[ ! -d $HOME/.vim/bundle/Vundle.vim ]]; then
 #   echo 'cloning Vundle.vim'
