90f7d482262d5c3c05df43e874d873f64c7da309
Nemanja Simeunovic <nem@nemsim.com>
2016-08-14 23:15:50 -0500

    cleaning up

--- bootstrap/tools.sh
+++ bootstrap/tools.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# pattern "borrowed" from
+# parts of this were borrowed from
 # https://github.com/tscheffe/dotfiles/blob/master/bootstrap/apps.sh
 program_exists() {
   type $1 > /dev/null 2>&1
@@ -8,17 +8,22 @@ program_exists() {
 
 echo 'bootstrapping tools'
 
-#if ! program_exists derp; then
-#  echo 'you need to install derp!'
-#  echo 'derp.co/install'
-#  exit 1
-#else
-#  echo 'you have this'
-#fi
+# ag - the silver searcher
+# https://github.com/ggreer/the_silver_searcher
+if ! program_exists ag; then
+  echo 'you need to install ag - the silver searcher'
+  echo 'see: https://github.com/ggreer/the_silver_searcher'
+  exit 1
+else
+  echo 'ag found'
+fi
 
-# if [[ ! -d $HOME/.vim/bundle/Vundle.vim ]]; then
-#   echo 'cloning Vundle.vim'
-#   git clone git@github.com:VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
-# fi
+# ruby manager?
+# TODO
+
+# thyme - pomodoro timer
+# (requires ruby)
+# https://github.com/hughbien/thyme
+# TODO
 
 echo 'finished'
