09d706ea8ab9a4801220d7592c6944f11bfee69e
Nemanja Simeunovic <nem@nemsim.com>
2016-09-10 04:23:21 -0500

    need this for ubuntu

--- bootstrap/debian-dependencies.sh
+++ bootstrap/debian-dependencies.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-sudo add-apt-repository ppa:martin-frost/thoughtbot-rcm
 sudo apt-get update
-sudo apt-get upgrade git vim tmux task rcm
+sudo apt-get upgrade git vim tmux task
 sudo apt-get install\
-  silversearcher-ag gcc\
-  bzip2 sqlite3 autoconf bison\
-  build-essential libssl-dev libyaml-dev libreadline6-dev\
-  zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev
+  silversearcher-ag gcc software-properties-common\
+  bzip2 sqlite3 autoconf bison build-essential\
+  libssl-dev libyaml-dev libreadline6-dev\
+  zlib1g-dev libncurses5-dev libffi-dev libgdbm3\
+  libgdbm-dev
 
+sudo add-apt-repository ppa:martin-frost/thoughtbot-rcm
+sudo apt-get install rcm
