1401e55c7d651b286fd83e16dc761888e6606cc1
Nemanja Simeunovic <nem@nemsim.com>
2016-09-10 04:18:29 -0500

    restructure how debian dependencies are installed

--- bootstrap/debian-dependencies.sh
+++ bootstrap/debian-dependencies.sh
@@ -2,9 +2,10 @@
 
 sudo add-apt-repository ppa:martin-frost/thoughtbot-rcm
 sudo apt-get update
-sudo apt-get upgrade\
-  git vim tmux silversearcher-ag task gcc\
-  bzip2 sqlite3 rcm gcc autoconf bison\
+sudo apt-get upgrade git vim tmux task rcm
+sudo apt-get install\
+  silversearcher-ag gcc\
+  bzip2 sqlite3 autoconf bison\
   build-essential libssl-dev libyaml-dev libreadline6-dev\
   zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev
 
