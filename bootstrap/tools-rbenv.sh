2d2d8c427f470d85b9a945ab8c4b9123ff3e5d07
Nemanja Simeunovic <nem@nemsim.com>
2016-09-11 02:46:36 -0500

    WIP - clean this up into a prompt

--- bootstrap/tools-rbenv.sh
+++ bootstrap/tools-rbenv.sh
@@ -28,9 +28,6 @@ if program_exists rbenv; then
   echo '... complete'
 
   echo '... rbenv-default-gems'
-  echo '... ... linking deafult gems file'
-  ln -sf $HOME/dotfiles/rbenv/default-gems $rbenv_path/default-gems
-  echo '... ... installing plugin'
   git clone https://github.com/sstephenson/rbenv-default-gems.git $rbenv_plugins_path/rbenv-default-gems
   echo '... complete'
 else
@@ -39,9 +36,12 @@ fi
 
 # install a newish ruby
 if program_exists rbenv; then
-  echo 'installing ruby 2.3.1'
-  rbenv install 2.3.1
-  rbenv global 2.3.1
+  # echo 'installing ruby 2.3.1'
+  echo 'set up your default gems file then do this'
+  echo 'rbenv install 2.3.1'
+  echo 'rbenv global 2.3.1'
+  # rbenv install 2.3.1
+  # rbenv global 2.3.1
 else
   echo 'rbenv not found?'
 fi
